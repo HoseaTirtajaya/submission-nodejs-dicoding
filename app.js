aef144c0a37c4847391c7a92b02e2834a0701120
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2021-05-03 16:28:47 +0700

    update

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const Hapi = require("@hapi/hapi")
+const http = require("http");
 const dotenv = require('dotenv').config();
 
 const init = async () => {
+    const routes = require("./routes");
+    // let server = http.createServer(routes);
+    
+
     const server = Hapi.server({
         port: process.env.PORT,
-        host: "localhost"
+        host: process.env.HOST
     });
 
-    await server.start().then(() => {
-        console.log(`Server is run at PORT ${process.env.port}`);
-    }).catch();
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: (request, h) => {
+            return h.response(`<h1 align="center">Welcome to Webserver(Tutorial by Dicoding) on ${server.info.uri}</h1>`).code(201)
+        }
+    });
+
+    await server.start();
+    console.log(`Server has run at ${server.info.uri}`)
 }
 
 init();
