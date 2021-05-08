e30b487d8ec1b37641a35153c47a674b0309ee85
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2021-05-08 20:46:31 +0700

    DONEgit add .

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const Hapi = require("@hapi/hapi");
-const { Console } = require("console");
-const http = require("http");
-const dotenv = require('dotenv').config();
+const Joi = require("joi");
+const dotenv = require('dotenv').config() 
 
 const booksController = require("./controllers/booksController");
 
 const init = async () => {
     const routes = require("./routes");
     // let server = http.createServer(routes);
-    
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -26,10 +24,32 @@ const init = async () => {
     server.route({
         method: "POST",
         path: "/books",
-        handler: (request, h) => {
-            let Books = booksController.createBooks(request, h);
-            // console.log(Books)
-            return h.response({Books})
+        config: {
+            validate: {
+                payload: Joi.object({
+                    name: Joi.string().required(),
+                    year: Joi.number().required(),
+                    author: Joi.string().required(),
+                    summary: Joi.string().required(),
+                    publisher: Joi.string().required(),
+                    pageCount: Joi.number().required(),
+                    readPage: Joi.number().required(),
+                    reading: Joi.boolean().required()
+                }),
+                
+            },
+
+            handler: (request, h) => {
+                let Books = booksController.createBooks(request, h);
+                // console.log(Books)
+                if(typeof(Books) === "string"){
+                    return h.response({status: "fail", message: Books}).code(400);
+                } else if(typeof(Books) === "object"){
+                    return h.response({Books}).code(201);
+                } else {
+                    return h.response({Books}).code(500)
+                }
+            }
         }
     });
 
@@ -38,7 +58,52 @@ const init = async () => {
         path: "/books",
         handler: (request, h) => {
             let Books = booksController.getBooks(request, h);
-            return h.response({message: "ah"});
+            return h.response(Books).code(201);
+        }
+    });
+
+    server.route({
+        method: "PUT",
+        path: "/books/{bookId}",
+        config: {
+            validate: {
+                payload: Joi.object({
+                    name: Joi.string().required(),
+                    year: Joi.number().required(),
+                    author: Joi.string().required(),
+                    summary: Joi.string().required(),
+                    publisher: Joi.string().required(),
+                    pageCount: Joi.number().required(),
+                    readPage: Joi.number().required(),
+                    reading: Joi.boolean().required()
+                }),
+                
+            },
+        },
+        handler: (request, h) => {
+            let Books = booksController.getSpecificBooks(request, h);
+
+            if(Books.message === "Gagal memperbaharui buku. Buku tidak ditemukan"){
+                return h.response(Books).code(404)
+            } else if(Books.message === "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"){
+                return h.response(Books).code(400)
+            } else if(Books){
+                return h.response(Books).code(200)
+            }
+        }
+    });
+
+    server.route({
+        method: "DELETE",
+        path: "/books/{bookId}",
+        handler: (request, h) => {
+            let Books = booksController.deleteBook(request, h);
+
+            if(Books.message === "Buku gagal dihapus. Id tidak ditemukan"){
+                return h.response(Books).code(404)
+            } else {
+                return h.response(Books).code(200)
+            }
         }
     });
 
--- controllers/booksController.js
+++ controllers/booksController.js
@@ -4,15 +4,122 @@ const Books = require("../models/Books");
 class booksController{
     static createBooks(request, h){
         let { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
-        let finished = pageCount === readPage ? true : false;
 
-        Books.push({id: nanoid(), name, year, author, summary, publisher, pageCount, readPage, reading, finished, insertedAt: new Date().toISOString(), updatedAt: new Date().toISOString()});
-        return Books;
+        if(pageCount >= readPage){
+            let finished = pageCount === readPage ? true : false;
+            let books_id = nanoid();
+    
+            Books.push({id: books_id, name, year, author, summary, publisher, pageCount, readPage, reading, finished, insertedAt: new Date().toISOString(), updatedAt: new Date().toISOString()});
+
+            let response = {status: "success", message: "Buku berhasil ditambahkan", data: {bookId: books_id}}
+            return response;
+        } else if(pageCount <= readPage){
+            return "Gagal menambahkan buku. readPage tidak boleh lebih besar dari total halaman"
+        } else {
+            let response = {
+                status: "error",
+                message: "buku gagal ditambahkan"
+            }
+
+            return response;
+        }
     }
 
     static getBooks(request, h){
-        console.log("masuk")
+        let books = [];
+        
+        Books.forEach(item => {
+            books.push({
+              id: item.id,
+              name: item.name,
+              publisher: item.publisher  
+            })
+        });
+        let response = {
+            status: "success",
+            data: {
+                books
+            }
+        }
+        return response;
     }
-}
+    
+    static getSpecificBooks(request, h) {
+        let {bookId} = request.params;
+        let {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
+        let found = false;
+
+        for(let i = 0; i < Books.length; i++){
+            if(Books[i].id === bookId){
+                if(pageCount >= readPage){
+                    let finished = pageCount === readPage ? true : false;
+
+                    Books[i].name = name;
+                    Books[i].year = year;
+                    Books[i].author = author;
+                    Books[i].summary = summary;
+                    Books[i].publisher = publisher;
+                    Books[i].pageCount = pageCount;
+                    Books[i].readPage = readPage;
+                    Books[i].reading = reading;
+                    Books[i].finished = finished;
+    
+                    let response = {
+                        status: "success",
+                        message: "Buku berhasil diperbaharui"
+                    }
+                    
+                    found = true;
+                    return response;
+                } else if(pageCount <= readPage){
+                    found == true;
+                    let response = {
+                        status: "fail",
+                        message: "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"
+                    }
+                    
+                    return response;
+                }
+            } 
+        }
+        if(found === false){
+                let response = {
+                    status: "fail",
+                    message: "Gagal memperbaharui buku. Buku tidak ditemukan"
+                }
+                
+                return response;
+        }
+    }
+
+    static deleteBook(request, h){
+        let {bookId} = request.params;
+        let found = false;
+        
+        for(let j = 0; j < Books.length; j++){
+            if(Books[j].id == bookId){
+                found = true;
+                Books.splice(j, 1);
+
+                let response = {
+                    status: "success",
+                    message: "Buku berhasil dihapus"
+                }
+
+                return response;
+            }
+        }
+        
+        if(found === false){
+            let response = {
+                status: "fail",
+                message: "Buku gagal dihapus. Id tidak ditemukan"
+            }
+
+            return response;
+        }
+    }
+
+    }
 
 module.exports = booksController;
